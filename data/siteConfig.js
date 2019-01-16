1e42ac868e4d6cd72b55873e8bf387128da8a218
euan-gwd <euan.gwd@gmail.com>
2019-01-16 15:23:54 +0200

    init repo

---
+++ data/siteConfig.js
@@ -0,0 +1,90 @@
+module.exports = {
+  siteTitle: 'Euan Greenwood',
+  siteDescription: `Online CV and Portfolio of Euan Greenwood`,
+  authorName: 'Euan Greenwood',
+  twitterUsername: 'euangwd',
+  githubUsername: 'euan-gwd',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `I am self-taught, started at an early age, professionally I started as a Microsoft systems and network administrator for a small manufacturing firm. I then moved on to do <strong>web design</strong> using Flash and Action Script and then 3d animation and visual effects using 3D Studio Max, before moving to technical support in the Apple industry, starting with an Apple retailer and service center doing both hardware repairs and software support and finally moving on to do corporate support. Recently moved back into <strong>web app development</strong>. Where I enjoy to building user interfaces using <strong>Javascript</strong> and <strong>React</strong> to solve interesting problems.`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 60,
+    },
+    {
+      name: 'CSS',
+      level: 70,
+    },
+    {
+      name: 'Javascript',
+      level: 80,
+    },
+    {
+      name: 'NodeJs',
+      level: 50,
+    },
+    {
+      name: 'React',
+      level: 80,
+    },
+    {
+      name: 'Git',
+      level: 50,
+    },
+  ],
+  jobs: [
+    {
+      company: 'canpango',
+      begin: {
+        month: 'jul',
+        year: '2018',
+      },
+      duration: '6 months',
+      occupation: 'Front-End developer',
+      description:
+        'Implemented front-end UI for various new features using ReactJS and Python. Most notably implemented a user and team management interface for an internal project called Talistry as well as implementing data visualization using D3 to show User and Team resource and time usage. Implemented Front-end testing enviroment',
+    },
+    {
+      company: 'Amwic Consulting',
+      begin: {
+        month: 'jan',
+        year: '2016',
+      },
+      duration: '2 yr & 6 mths',
+      occupation: 'Front-End developer',
+      description:
+        'Development and maintenance, corrective and preventive, of web applications for the small business market.',
+    },
+    {
+      company: 'Amwic Consulting',
+      begin: {
+        month: 'jun',
+        year: '2013',
+      },
+      duration: '3 yrs',
+      occupation: 'Senior Network and Computer Systems Engineer',
+      description:
+        'Responsible for the implementation of the Cloud based Infrastructure systems, training and customer support. assisted with all facets of IT systems maintenance',
+    },
+  ],
+  social: {
+    twitter: 'https://twitter.com/euangwd',
+    linkedin: 'https://www.linkedin.com/in/euangreenwood',
+    github: 'https://github.com/euan-gwd',
+    email: 'contact@euangreenwood.com',
+  },
+  siteUrl: 'https://euangreenwood.com',
+  pathPrefix: '/', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: '',
+  background_color: '#ffffff',
+  theme_color: '#003366',
+  display: 'minimal-ui',
+  icon: 'src/assets/eg-icon.png',
+  headerLinks: [
+    {
+      label: 'Euan Greenwood',
+      url: '/',
+    },
+  ],
+}
